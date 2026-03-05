83e05d2ea09f756c818b99f523844c374ae64d6e
Dzkzzz <dzdzaakk@gmail.com>
2026-03-05 22:05:39 +0700

    variables

---
+++ 01-pythonRefresher/Variables.py
@@ -0,0 +1,27 @@
+"""
+Variables
+"""
+
+cost = 10
+tax_percent = 0.25
+tax = cost * tax_percent
+price = cost + tax
+
+print(price)
+
+first_name = "Dzak"
+print(first_name)
+
+first_name = "Bob"
+print(first_name)
+
+
+first_num = 10
+second_num = 2
+
+print(first_num)
+print(second_num)
+
+first_num = 1
+print(first_num)
+print(second_num)
