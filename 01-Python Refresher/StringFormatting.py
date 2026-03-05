b5fddd710e60e4f38aa33ee908c30afb9863e585
Dzkzzz <dzdzaakk@gmail.com>
2026-03-05 23:34:31 +0700

    string formatting

---
+++ 01-Python Refresher/StringFormatting.py
@@ -0,0 +1,10 @@
+"""
+String Formatting
+"""
+
+sentence = "Hi {} {}"
+first_name = "Dzak"
+last_name = "Peter"
+print(sentence.format(first_name, last_name))
+
+print(f"Hi {first_name} {last_name} I hope you are learning")
