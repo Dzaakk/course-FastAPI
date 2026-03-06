78a63e26e0657d5f9017d4b7081bb0244841fe9d
Dzkzzz <dzdzaakk@gmail.com>
2026-03-06 21:06:57 +0700

    if else statement

---
+++ 01-Python Refresher/IfElse.py
@@ -0,0 +1,17 @@
+x = 2
+
+if x > 1:
+    print("x is greater than 1")
+else:
+    print("x is not greater than 1")
+
+print("Outside of if statement")
+
+hour = 13
+
+if hour < 15:
+    print("Good morning!")
+elif hour < 20:
+    print("Good afternoon!")
+else:
+    print("Good night!")
