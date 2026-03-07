7f6b7d916b07ef435148218da62f6dd1720ab3f7
Dzkzzz <dzdzaakk@gmail.com>
2026-03-07 17:17:36 +0700

    constructors

--- 01-Python Refresher/OOP/main.py
+++ 01-Python Refresher/OOP/main.py
@@ -1,8 +1,12 @@
 from Enemy import *
 
-zombie = Enemy()
-zombie.type_of_enemy = "Zombie"
+zombie = Enemy("Zombie", 10, 1)
+big_zombie = Enemy("Big Zombie", 50, 5)
 
 print(zombie.talk())
 print(zombie.walk_forward())
 print(zombie.attack())
+
+print(big_zombie.talk())
+print(big_zombie.walk_forward())
+print(big_zombie.attack())
