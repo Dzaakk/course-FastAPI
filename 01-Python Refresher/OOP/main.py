2083289fb99ba5a5903fccacf068f0d2575abd07
Dzkzzz <dzdzaakk@gmail.com>
2026-03-09 06:54:11 +0700

    oop encapsulation

--- 01-Python Refresher/OOP/main.py
+++ 01-Python Refresher/OOP/main.py
@@ -1,12 +1,14 @@
 from Enemy import *
 
 zombie = Enemy("Zombie", 10, 1)
-big_zombie = Enemy("Big Zombie", 50, 5)
+# big_zombie = Enemy("Big Zombie", 50, 5)
 
-print(zombie.talk())
-print(zombie.walk_forward())
-print(zombie.attack())
+print(zombie.get_type_of_enemy())
 
-print(big_zombie.talk())
-print(big_zombie.walk_forward())
-print(big_zombie.attack())
+# print(zombie.talk())
+# print(zombie.walk_forward())
+# print(zombie.attack())
+
+# print(big_zombie.talk())
+# print(big_zombie.walk_forward())
+# print(big_zombie.attack())
